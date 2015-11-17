eb5f92049c98a81382c7d10bda1a21361f90766d
Jonathan Wood <jonathanwood@jonathans-mbp.lan>
2015-11-17 15:03:41 +0000

    setting up new test for tile map in Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,25 +4,20 @@
 #include "TileMap.h"
 #include "AssetLoader.h"
 #include "Camera.h"
+#include <unistd.h>
+#include <stdio.h>
+
 
 int main() {
-	 
 	sf::RenderWindow gameWindow(sf::VideoMode(1024,1024), "Antificial Antelligence - [PRE-ALPHA]");
 	
 	sf::Time deltaTime = sf::seconds(1.0f/60.0f);
 
 	sf::Clock clock;
 
-	TileMap tileMap = TileMap();
-	tileMap.setMapSize(200,200);
-	tileMap.setTileSize(256);
-
 	AssetLoader::loadTextures(); 
 
-	std::vector<sf::Sprite> spriteVector{
-		AssetLoader::getSpriteEarth(), 
-		AssetLoader::getSpriteEarthHorizontalTunnel()
-	};
+
 
 	sf::View view;
 
@@ -30,13 +25,33 @@ int main() {
 	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 	gameWindow.setView(view);
 
-	tileMap.setTileSprites(spriteVector);
+    
+    //Set up tilemap
+    std::vector<TileData> tileDataArray{
+        TileData(AssetLoader::getSpriteEarth()),
+        TileData(AssetLoader::getSpriteEarthHorizontalTunnel())
+    };
+    
+    tileDataArray[1].localPathNetwork.createNewNode(sf::Vector2f(0, 154));
+    tileDataArray[1].localPathNetwork.createNewNodeConnectedTo(sf::Vector2f(255, 154), 0);
+    
+    TileMap tileMap = TileMap();
+    tileMap.setMapSize(200,200);
+    tileMap.setTileSize(256);
 	tileMap.setMapPosition(0,0);
-
-	tileMap.randomizeMap();
-
+    tileMap.setTileData(tileDataArray);
+    //Finish Tilemap setup
+    
+    
 	Ant ant = Ant(sf::Vector2f(100, 100));
-
+    
+    sf::Sprite testSprite;
+    sf::Texture testTex;
+    testTex.loadFromFile("madAnt.png");
+    testSprite.setTexture(testTex);
+    
+    testSprite.setPosition(400, 300);
+    
 	while (gameWindow.isOpen()) {
 		 
 		sf::Event event;
@@ -52,12 +67,13 @@ int main() {
 
 		tileMap.draw(gameWindow, view );
 		ant.draw(gameWindow);
+        
+        gameWindow.draw(testSprite);
 
         updateCamera(deltaTime.asSeconds(), view, gameWindow);
         gameWindow.setView(view);
 		gameWindow.display();
 		deltaTime = clock.restart();
-
 	}
 
 	return 0;
