7f16ac345a612c3e6b327bc88289c655c5e0b4f4
David Wood <Dwood023@live.co.uk>
2015-11-20 18:26:09 +0000

    Added Earth.png

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,14 +1,12 @@
 #include "Animation.h"
+#include "AssetLoader.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "AssetLoader.h"
 
 Animation::Animation(std::vector<sf::IntRect> newFrames, float newFrameRate) {
 
 	frames = newFrames;
-
 	frameRate = newFrameRate;
-
 	frameCounter = 0;
 
 }
@@ -18,16 +16,14 @@ void Animation::update(sf::Sprite& spriteToAnimate, float deltaTime) {
 	timeSinceLastAnimFrame += deltaTime;
 
 	if (timeSinceLastAnimFrame >= 1.0f / frameRate) {
-	
-		timeSinceLastAnimFrame = 0;	
-
-		frameCounter++;
 
 		if (frameCounter >= frames.size()) {
 			 frameCounter = 0;
 		}
 
-		spriteToAnimate.setTextureRect(frames[frameCounter]);	 
+		spriteToAnimate.setTextureRect(frames[frameCounter++]);	 
+
+		timeSinceLastAnimFrame = 0;	
 
 	}
 
